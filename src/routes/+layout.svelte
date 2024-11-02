9a7ca12eafdaec1a8301960cbe387ed865dd59cd
Tim Buser <102681999+tbuserdev@users.noreply.github.com>
2024-11-02 22:58:26 +0000

    add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,7 @@
 		content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
 	/>
 	<link rel="icon" type="image/svg+xml" href="favicon.svg" />
+	<script defer data-domain="bloodpressure.tbuser.dev" src="https://plausible.tbuser.dev/js/script.js"></script>
 </svelte:head>
 
 <div class="flex h-screen items-center justify-center bg-[#151518]">
